794d1d9d30c260418ec4c42c4de6e2815732bfd3
julemache <julien1992@live.fr>
2018-10-26 11:04:18 +0200

    update2

--- forum/client/js/article.js
+++ forum/client/js/article.js
@@ -3,6 +3,8 @@ let app = new Vue({
   el: '#app',
   data: {
     articles: [],
+    titre: this.titre,
+    user: '1',
   },
   mounted() {
     this.getarticle();
@@ -16,17 +18,26 @@ let app = new Vue({
             // error callback
       });
     },
-  },
+    ajouter() {
+      this.$http.post('http://localhost:3000/api/articles', {title: this.titre, datecreation: new Date(Date.now()), accountId: this.user}).then((response) => {
+        this.articles = response.data;
+      }, (response) => {
+        console.log('erreur', response);
+      });
+      this.titre = '';
+      this.getarticle();
+    },
+    supprimer(id) {
+      this.$http.delete('http://localhost:3000/api/articles/' + id).then((response) => {
+        this.articles = response.data;
+      }, (response) => {
+        console.log('erreur', response);
+      });
+      this.getarticle();
+    },
+    modifier() {
 
-  ajouter() {
-    this.$http.post('http://localhost:3000/api/articles', {title: this.titre, datecreation: new Date(Date.now())});
-    this.getarticle();
-  },
-  supprimer() {
-
-  },
-  modifier() {
-
+    },
   },
 
 });
